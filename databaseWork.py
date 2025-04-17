7c71976bc4adb16ac0b3be25c230c7933560304b
Victor <v.saparbaev@mail.ru>
2025-04-17 16:00:28 +0300

    1
    1

--- databaseWork.py
+++ databaseWork.py
@@ -17,3 +17,20 @@ def start(message):
         user_id = [message.chat.id]
         dbCursor.execute("INSERT INTO login_id VALUES(?);", user_id)
         database.commit()
+
+def saveTheme(message):
+    database = sqlite3.connect('users.db')
+    dbCursor = database.cursor()
+    dbCursor.execute("""CREATE TABLE IF NOT EXISTS themes(
+	    name char,
+	    user_id INTEGER
+    )""")
+
+    database.commit()
+
+    theme = message.text
+    user_id = message.chat.id
+    dbCursor.execute(f"""INSERT INTO themes (name, user_id) VALUES("{theme}", {user_id})""")
+    database.commit()
+
+    #CONSTRAINT user_id_fk FOREIGN KEY (user_id) REFERENCES login_id (id)
--- telegramBot.py
+++ telegramBot.py
@@ -2,33 +2,44 @@ import telebot
 import databaseWork
 from telebot import types
 
+<<<<<<< Updated upstream
 bot = telebot.TeleBot('key')
+=======
+bot = telebot.TeleBot('7780379541:AAHjpvmXg4-VDoENfEk6jvolXSaw5tM58Ao')
+>>>>>>> Stashed changes
 
 @bot.message_handler(commands=['start'])
 def start(message):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
     button0 = types.KeyboardButton('Мои темы')
-    button1 = types.KeyboardButton('Добавить тему')
+    button1 = types.KeyboardButton('Новая тема')
     markup.add(button0)
     markup.add(button1)
     databaseWork.start(message)
-    bot.send_message(message.chat.id, 'Привет!', reply_markup=markup)
+    bot.send_message(message.chat.id, 'Привет, ' + message.from_user.first_name + '!')
+    bot.send_message(message.chat.id, 'Добро пожаловать в бот "Дневник записей"!', reply_markup=markup)
     bot.register_next_step_handler(message, OnClick)
 
 def OnClick(message):
     if message.text == 'Мои темы':
-        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-        button0 = types.KeyboardButton('Личная тема')
-        button1 = types.KeyboardButton('Общая тема')
-        markup.add(button0)
-        markup.add(button1)
-        bot.send_message(message.chat.id, message.from_user.id, reply_markup=markup)
-    elif message.text == 'Добавить тему':
-        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-        button0 = types.KeyboardButton('Личная тема')
-        button1 = types.KeyboardButton('Общая тема')
-        markup.add(button0)
-        markup.add(button1)
-        bot.send_message(message.chat.id, message.from_user.id, reply_markup=markup)
+        bot.send_message(message.chat.id, 'тут короче эээ лан иди нахуй' )
+    elif message.text == 'Новая тема':
+        markup = types.ReplyKeyboardRemove()
+        bot.send_message(message.chat.id, message.from_user.first_name + ', напишите название темы', reply_markup=markup)
+        bot.register_next_step_handler(message, OnCreateNewTheme)
+
+def OnCreateNewTheme(message):
+    databaseWork.saveTheme(message)
+    bot.send_message(message.chat.id, 'Ваша тема ' + message.text + ' записана. Добавьте первые заметки или задачи.')
+    bot.register_next_step_handler(message, OnCreateNewEntry)
+
+def OnCreateNewEntry(message):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    button0 = types.KeyboardButton('Мои темы')
+    button1 = types.KeyboardButton('Новая тема')
+    markup.add(button0)
+    markup.add(button1)
+    bot.send_message(message.chat.id, 'Запись добавлена', reply_markup=markup)
+    bot.register_next_step_handler(message, OnClick)
 
 bot.polling(non_stop=True)
