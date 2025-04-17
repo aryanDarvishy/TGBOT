5b9ccd1fa289d41f97987a9fd851288ae4708016
Victor <v.saparbaev@mail.ru>
2025-04-17 16:00:34 +0300

    1
    1

--- telegramBot.py
+++ telegramBot.py
@@ -3,7 +3,11 @@ import databaseWork
 from telebot import types
 
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 bot = telebot.TeleBot('key')
+=======
+bot = telebot.TeleBot('7780379541:AAHjpvmXg4-VDoENfEk6jvolXSaw5tM58Ao')
+>>>>>>> Stashed changes
 =======
 bot = telebot.TeleBot('7780379541:AAHjpvmXg4-VDoENfEk6jvolXSaw5tM58Ao')
 >>>>>>> Stashed changes
@@ -27,6 +31,23 @@ def OnClick(message):
         markup = types.ReplyKeyboardRemove()
         bot.send_message(message.chat.id, message.from_user.first_name + ', напишите название темы', reply_markup=markup)
         bot.register_next_step_handler(message, OnCreateNewTheme)
+<<<<<<< Updated upstream
+=======
+
+def OnCreateNewTheme(message):
+    databaseWork.saveTheme(message)
+    bot.send_message(message.chat.id, 'Ваша тема ' + message.text + ' записана. Добавьте первые заметки или задачи.')
+    bot.register_next_step_handler(message, OnCreateNewEntry)
+
+def OnCreateNewEntry(message):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    button0 = types.KeyboardButton('Мои темы')
+    button1 = types.KeyboardButton('Новая тема')
+    markup.add(button0)
+    markup.add(button1)
+    bot.send_message(message.chat.id, 'Запись добавлена', reply_markup=markup)
+    bot.register_next_step_handler(message, OnClick)
+>>>>>>> Stashed changes
 
 def OnCreateNewTheme(message):
     databaseWork.saveTheme(message)
